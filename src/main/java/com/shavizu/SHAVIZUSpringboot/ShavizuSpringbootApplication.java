4580436af707ac306b48184098081873d46712e6
이승윤 <yyuunn17@naver.com>
2022-03-23 14:55:15 +0900

    📑 :: init

---
+++ src/main/java/com/shavizu/SHAVIZUSpringboot/ShavizuSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.shavizu.SHAVIZUSpringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShavizuSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShavizuSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shavizu/SHAVIZUSpringboot/ShavizuSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shavizu.SHAVIZUSpringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShavizuSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

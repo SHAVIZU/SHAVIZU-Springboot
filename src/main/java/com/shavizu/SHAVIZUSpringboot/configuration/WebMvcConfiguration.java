cdf14d9b0be818e8b13f240ab0a5e5c0fdab517d
이승윤 <yyuunn17@naver.com>
2022-03-30 23:21:04 +0900

    ⚙️ :: (#2) WebMvcConfiguration

---
+++ src/main/java/com/shavizu/SHAVIZUSpringboot/configuration/WebMvcConfiguration.java
@@ -0,0 +1,20 @@
+package com.shavizu.SHAVIZUSpringboot.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@EnableWebMvc
+@Configuration
+public class WebMvcConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("OPTIONS", "HEAD", "GET", "POST", "PUT", "PATCH", "DELETE")
+                .allowedHeaders("*");
+    }
+
+}

c7e293bb70755129086fe12570eac868ba06686d
이승윤 <yyuunn17@naver.com>
2022-04-01 10:50:46 +0900

    📑 :: (#4) BaseEntity 추가

---
+++ src/main/java/com/shavizu/SHAVIZUSpringboot/entity/BaseIdEntity.java
@@ -0,0 +1,21 @@
+package com.shavizu.SHAVIZUSpringboot.entity;
+
+import lombok.Getter;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.EntityListeners;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseIdEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+}

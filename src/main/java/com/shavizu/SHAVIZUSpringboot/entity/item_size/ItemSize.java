3973f241788aff05a1cf3d125ae2f0158d067f8c
이승윤 <yyuunn17@naver.com>
2022-04-02 23:33:09 +0900

    📑 :: (#4) ItemSize entity

---
+++ src/main/java/com/shavizu/SHAVIZUSpringboot/entity/item_size/ItemSize.java
@@ -0,0 +1,36 @@
+package com.shavizu.SHAVIZUSpringboot.entity.item_size;
+
+import com.shavizu.SHAVIZUSpringboot.entity.BaseIdEntity;
+import com.shavizu.SHAVIZUSpringboot.entity.item.Item;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name = "tbl_item_size")
+public class ItemSize extends BaseIdEntity {
+
+    @Column(length = 10, nullable = false)
+    private String size;
+
+    @ManyToOne
+    @JoinColumn(name = "item_id", nullable = false)
+    private Item item;
+
+    public static ItemSize createItemSize(String size, Item item) {
+        ItemSize itemSize = new ItemSize();
+        itemSize.size = size;
+        itemSize.item = item;
+
+        return itemSize;
+    }
+
+}
---
+++ src/main/java/com/shavizu/SHAVIZUSpringboot/entity/item_size/repository/ItemSizeRepository.java
@@ -0,0 +1,7 @@
+package com.shavizu.SHAVIZUSpringboot.entity.item_size.repository;
+
+import com.shavizu.SHAVIZUSpringboot.entity.item_size.ItemSize;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ItemSizeRepository extends CrudRepository<ItemSize, Long> {
+}

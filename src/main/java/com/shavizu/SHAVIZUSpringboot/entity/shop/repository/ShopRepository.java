6cce380604ce09b9d8cd23ad7d1dce9231d74109
이승윤 <yyuunn17@naver.com>
2022-04-02 16:59:44 +0900

    ⚡️ :: findByUserId() 추가

--- src/main/java/com/shavizu/SHAVIZUSpringboot/entity/shop/repository/ShopRepository.java
+++ src/main/java/com/shavizu/SHAVIZUSpringboot/entity/shop/repository/ShopRepository.java
@@ -3,5 +3,8 @@ package com.shavizu.SHAVIZUSpringboot.entity.shop.repository;
 import com.shavizu.SHAVIZUSpringboot.entity.shop.Shop;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface ShopRepository extends CrudRepository<Shop, Long> {
+    Optional<Shop> findByUserId(String userId);
 }

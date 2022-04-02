b2e304fb18404284530a5cf03ad02a2d94538c19
이승윤 <yyuunn17@naver.com>
2022-04-02 23:49:18 +0900

    📑 :: (#4) Sell entity

---
+++ src/main/java/com/shavizu/SHAVIZUSpringboot/entity/sell/Sell.java
@@ -0,0 +1,54 @@
+package com.shavizu.SHAVIZUSpringboot.entity.sell;
+
+import com.shavizu.SHAVIZUSpringboot.entity.BaseIdCreatedAtEntity;
+import com.shavizu.SHAVIZUSpringboot.entity.item.Item;
+import com.shavizu.SHAVIZUSpringboot.entity.shop.Shop;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name = "tbl_sell",
+        uniqueConstraints = {
+            @UniqueConstraint(columnNames = {"shop_id", "item_id"})
+})
+public class Sell extends BaseIdCreatedAtEntity {
+
+    @Column(nullable = false)
+    private Integer price;
+
+    @Column(nullable = false)
+    private Integer discountRate;
+
+    @Column(nullable = false)
+    private Integer discountPrice;
+
+    @ManyToOne
+    @JoinColumn(name = "shop_id")
+    private Shop shop;
+
+    @ManyToOne
+    @JoinColumn(name = "item_id")
+    private Item item;
+
+    public static Sell createSell(Integer price, Integer discountRate, Shop shop, Item item) {
+        Sell sell = new Sell();
+        sell.price = price;
+        sell.discountRate = discountRate;
+        sell.discountPrice = price * (100 - discountRate);
+        sell.shop = shop;
+        sell.item = item;
+
+        return sell;
+    }
+
+}
---
+++ src/main/java/com/shavizu/SHAVIZUSpringboot/entity/sell/repository/SellRepository.java
@@ -0,0 +1,7 @@
+package com.shavizu.SHAVIZUSpringboot.entity.sell.repository;
+
+import com.shavizu.SHAVIZUSpringboot.entity.sell.Sell;
+import org.springframework.data.repository.CrudRepository;
+
+public interface SellRepository extends CrudRepository<Sell, Long> {
+}

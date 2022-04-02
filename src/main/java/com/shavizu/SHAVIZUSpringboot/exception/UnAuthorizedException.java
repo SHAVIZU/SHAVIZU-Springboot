76f0d840d5d39a3a5476504c2d4f26fadf3b9037
이승윤 <yyuunn17@naver.com>
2022-04-02 17:07:26 +0900

    📑 :: UnAuthorizedException 추가

---
+++ src/main/java/com/shavizu/SHAVIZUSpringboot/exception/UnAuthorizedException.java
@@ -0,0 +1,16 @@
+package com.shavizu.SHAVIZUSpringboot.exception;
+
+import com.shavizu.SHAVIZUSpringboot.exception.handler.ShavizuException;
+import org.springframework.http.HttpStatus;
+
+public class UnAuthorizedException extends ShavizuException {
+
+    public static final UnAuthorizedException INVALID_TOKEN = new UnAuthorizedException("Invalid token");
+
+    public static final UnAuthorizedException EXPIRED_TOKEN = new UnAuthorizedException("Expired token");
+
+    private UnAuthorizedException(String message) {
+        super(HttpStatus.UNAUTHORIZED.value(), message);
+    }
+
+}

f2b1dbcdd61df39cb90d7707a966aa671ce2a326
이승윤 <yyuunn17@naver.com>
2022-04-03 15:13:00 +0900

    ♻️ :: 파일명 변경

--- src/main/java/com/shavizu/SHAVIZUSpringboot/exception/handler/ShavizuExceptionHandlingFilter.java
+++ src/main/java/com/shavizu/SHAVIZUSpringboot/exception/handler/ShavizuExceptionHandlingFilter.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-public class ShavizuExceptionHandleringFilter extends OncePerRequestFilter {
+public class ShavizuExceptionHandlingFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {

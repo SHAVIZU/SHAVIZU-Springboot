a6b96b6e2ebb56b160ab0400b6fff68ff1b76310
이승윤 <yyuunn17@naver.com>
2022-04-03 15:19:44 +0900

    📑 :: controller 클래스 추가

---
+++ src/main/java/com/shavizu/SHAVIZUSpringboot/controller/SellsController.java
@@ -0,0 +1,11 @@
+package com.shavizu.SHAVIZUSpringboot.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RequestMapping("/sells")
+@RestController
+public class SellsController {
+}

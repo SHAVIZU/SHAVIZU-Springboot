3216dce3097963a7cd2220f1261d9a8ea638a7ef
이승윤 <yyuunn17@naver.com>
2022-04-04 10:36:01 +0900

    📑 :: (#18) 상세주소, 위경도 칼럼 추가

--- src/main/java/com/shavizu/SHAVIZUSpringboot/entity/shop_information/ShopInformation.java
+++ src/main/java/com/shavizu/SHAVIZUSpringboot/entity/shop_information/ShopInformation.java
@@ -12,6 +12,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.MapsId;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
+import java.math.BigDecimal;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -28,24 +29,36 @@ public class ShopInformation {
     @Column(length = 100, nullable = false)
     private String address;
 
+    @Column(length = 50, nullable = false)
+    private String detailedAddress;
+
     @Column(length = 200, nullable = false)
     private String openingHours;
 
     @Column(length = 150)
     private String description;
 
+    @Column(columnDefinition = "decimal(10,8)", nullable = false)
+    private BigDecimal latitude;
+
+    @Column(columnDefinition = "decimal(11,8)", nullable = false)
+    private BigDecimal longitude;
+
     @MapsId
     @OneToOne
     @JoinColumn(name = "shop_id", nullable = false)
     private Shop shop;
 
-    public static ShopInformation createShopInformation(Shop shop, String telephone, String address, String openingHours, String description) {
+    public static ShopInformation createShopInformation(Shop shop, String telephone, String address, String detailedAddress, String openingHours, String description, BigDecimal latitude, BigDecimal longitude) {
         ShopInformation shopInformation = new ShopInformation();
         shopInformation.shop = shop;
         shopInformation.telephone = telephone;
         shopInformation.address = address;
+        shopInformation.detailedAddress = detailedAddress;
         shopInformation.openingHours = openingHours;
         shopInformation.description = description;
+        shopInformation.latitude = latitude;
+        shopInformation.longitude = longitude;
 
         return shopInformation;
     }

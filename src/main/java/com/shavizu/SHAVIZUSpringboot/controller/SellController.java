56c07f6e8376def7d640fb75f77f3b4312b07a9f
이승윤 <yyuunn17@naver.com>
2022-04-13 15:44:51 +0900

    🐛 :: (#30) response status 변경

--- src/main/java/com/shavizu/SHAVIZUSpringboot/controller/SellController.java
+++ src/main/java/com/shavizu/SHAVIZUSpringboot/controller/SellController.java
@@ -5,11 +5,13 @@ import com.shavizu.SHAVIZUSpringboot.dto.response.SellDetailsResponse;
 import com.shavizu.SHAVIZUSpringboot.service.inventory.UpdateInventoryService;
 import com.shavizu.SHAVIZUSpringboot.service.sell.GetSellDetailsService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
@@ -27,6 +29,7 @@ public class SellController {
         return getSellDetailsService.execute(sellId);
     }
 
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     @PatchMapping("/inventory/{sell_id}")
     public void updateInventory(@PathVariable("sell_id") Long sellId, @RequestBody @Valid UpdateInventoryRequest request) {
         updateInventoryService.execute(sellId, request);

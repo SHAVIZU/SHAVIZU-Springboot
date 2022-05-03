0db01798cabb43394cfd65b5407d1c8ef1546402
이승윤 <yyuunn17@naver.com>
2022-05-03 10:35:45 +0900

    🐛 :: (#40) 🐛🐛🐛🐛 property-naming-strategy 설정 오류 해결!

--- src/main/java/com/shavizu/SHAVIZUSpringboot/configuration/WebMvcConfiguration.java
+++ src/main/java/com/shavizu/SHAVIZUSpringboot/configuration/WebMvcConfiguration.java
@@ -2,10 +2,8 @@ package com.shavizu.SHAVIZUSpringboot.configuration;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@EnableWebMvc
 @Configuration
 public class WebMvcConfiguration implements WebMvcConfigurer {
 

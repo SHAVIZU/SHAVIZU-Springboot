bcc5f477a841d9439df497accec04e7fc9ab1bab
이승윤 <yyuunn17@naver.com>
2022-06-08 20:19:18 +0900

    ⚡️ :: (#50) findAllByShop queryDsl

--- src/main/java/com/shavizu/SHAVIZUSpringboot/entity/sell/repository/SellRepositoryExtension.java
+++ src/main/java/com/shavizu/SHAVIZUSpringboot/entity/sell/repository/SellRepositoryExtension.java
@@ -1,7 +1,12 @@
 package com.shavizu.SHAVIZUSpringboot.entity.sell.repository;
 
 import com.shavizu.SHAVIZUSpringboot.dto.response.SellDetailsResponse;
+import com.shavizu.SHAVIZUSpringboot.dto.response.SellsDto;
+import com.shavizu.SHAVIZUSpringboot.entity.shop.Shop;
+
+import java.util.List;
 
 public interface SellRepositoryExtension {
     SellDetailsResponse findBySellId(long sellId);
+    List<SellsDto> findAllByShop(Shop shop);
 }
--- src/main/java/com/shavizu/SHAVIZUSpringboot/entity/sell/repository/SellRepositoryImpl.java
+++ src/main/java/com/shavizu/SHAVIZUSpringboot/entity/sell/repository/SellRepositoryImpl.java
@@ -5,8 +5,13 @@ import com.shavizu.SHAVIZUSpringboot.dto.response.InventoryDto;
 import com.shavizu.SHAVIZUSpringboot.dto.response.ItemDto;
 import com.shavizu.SHAVIZUSpringboot.dto.response.SellDetailsResponse;
 import com.shavizu.SHAVIZUSpringboot.dto.response.SellDto;
+import com.shavizu.SHAVIZUSpringboot.dto.response.SellInventoryDto;
+import com.shavizu.SHAVIZUSpringboot.dto.response.SellsDto;
+import com.shavizu.SHAVIZUSpringboot.entity.shop.Shop;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 import static com.querydsl.core.types.Projections.constructor;
 import static com.querydsl.core.types.Projections.list;
@@ -15,6 +20,7 @@ import static com.shavizu.SHAVIZUSpringboot.entity.inventory.QInventory.inventor
 import static com.shavizu.SHAVIZUSpringboot.entity.item.QItem.item;
 import static com.shavizu.SHAVIZUSpringboot.entity.item_size.QItemSize.itemSize;
 import static com.shavizu.SHAVIZUSpringboot.entity.sell.QSell.sell;
+import static com.shavizu.SHAVIZUSpringboot.entity.shop.QShop.shop;
 
 @RequiredArgsConstructor
 @Repository
@@ -53,6 +59,33 @@ public class SellRepositoryImpl implements SellRepositoryExtension {
                 .join(sell.inventories, inventory)
                 .join(inventory.itemSize, itemSize)
                 .fetchOne();
+    }
+
+    @Override
+    public List<SellsDto> findAllByShop(Shop s) {
+        return jpaQuery.select(
+                        constructor(
+                                SellsDto.class,
+                                sell.discountPrice,
+                                item.name,
+                                item.imageUrl,
+                                brand.name,
+                                list(
+                                        constructor(
+                                                SellInventoryDto.class,
+                                                itemSize.size,
+                                                inventory.amount
+                                        )
+                                )
+                        )
+                )
+                .from(sell)
+                .join(sell.item, item)
+                .join(sell.shop, shop).where(shop.eq(s))
+                .join(item.brand, brand)
+                .join(sell.inventories, inventory)
+                .join(inventory.itemSize, itemSize)
+                .fetch();
     }
 
 }

77eeaea3905c9167dddde6674c46b2f09d8de4b7
이승윤 <yyuunn17@naver.com>
2022-06-20 15:24:27 +0900

    🐛 :: 임시 방편 / 상품 목록이 item size마다 따로 나가는 버그 수정

--- src/main/java/com/shavizu/SHAVIZUSpringboot/service/sell/GetSellsService.java
+++ src/main/java/com/shavizu/SHAVIZUSpringboot/service/sell/GetSellsService.java
@@ -1,7 +1,10 @@
 package com.shavizu.SHAVIZUSpringboot.service.sell;
 
+import com.shavizu.SHAVIZUSpringboot.dto.response.InventoryDto;
 import com.shavizu.SHAVIZUSpringboot.dto.response.SellsDto;
 import com.shavizu.SHAVIZUSpringboot.dto.response.SellsResponse;
+import com.shavizu.SHAVIZUSpringboot.entity.inventory.repository.InventoryRepository;
+import com.shavizu.SHAVIZUSpringboot.entity.sell.Sell;
 import com.shavizu.SHAVIZUSpringboot.entity.sell.repository.SellRepository;
 import com.shavizu.SHAVIZUSpringboot.entity.shop.Shop;
 import com.shavizu.SHAVIZUSpringboot.facade.AuthenticationFacade;
@@ -10,6 +13,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
@@ -17,14 +21,37 @@ import java.util.List;
 public class GetSellsService {
 
     private final SellRepository sellRepository;
+    private final InventoryRepository inventoryRepository;
 
     private final AuthenticationFacade authenticationFacade;
 
     public SellsResponse execute() {
         Shop shop = authenticationFacade.getShop();
 
-        List<SellsDto> sells = sellRepository.findAllByShop(shop);
-        return new SellsResponse(sells);
+        //List<SellsDto> sells = sellRepository.findAllByShop(shop);
+
+        List<Sell> sells = sellRepository.findByShop(shop);
+
+
+        return new SellsResponse(sells.stream().map(
+                s -> new SellsDto(
+                            s.getDiscountPrice(),
+                            s.getDiscountRate(),
+                            s.getItem().getName(),
+                            s.getItem().getImageUrl(),
+                            s.getItem().getBrand().getName(),
+                            s.getInventories().stream().map(
+                                    i -> new InventoryDto(
+                                                new InventoryDto.InventoryId(
+                                                        i.getSellId(),
+                                                        i.getItemSizeId()
+                                                ),
+                                                i.getItemSize().getSize(),
+                                                i.getAmount()
+                                        )
+                            ).collect(Collectors.toList())
+                    )
+        ).collect(Collectors.toList()));
     }
 
 }
